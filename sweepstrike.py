12ec9dcffd88173011edab154a56b62f45e3c7cc
truetheo <tburdach@gmail.com>
2018-06-02 10:21:39 +0200

    Comments to english

--- sweepstrike.py
+++ sweepstrike.py
@@ -5,14 +5,14 @@ import itertools
 import time
 
 
-def czytaj_kraje():
-    filez = open(r'D:\python_scripts\kraje.txt', 'r')
+def read_countries():
+    filez = open(r'D:\python_scripts\countries.txt', 'r')
     countries = filez.read().splitlines()
     filez.close()
     return countries
 
 
-def zapiszOsoby():
+def save_names():
     with open("names.txt", "w") as text_file:
         for i in range(1, 33):
             print(f"Osoba {str(i)}", file=text_file)
@@ -20,47 +20,43 @@ def zapiszOsoby():
     print("Writing done!")
 
 
-def czytaj_osoby():
+def read_names():
     filez = open(r'D:\python_scripts\names.txt', 'r')
     names = filez.read().splitlines()
     filez.close()
     return names
 
 
-def lista1(ludzie, kraje):
-    print("Rozpoczynam loswanie!")
-    for osoba, kraj in itertools.product(ludzie, kraje):
-        print(osoba + " wylosowala - " + kraj)
-    print("Koniec losowania!")
-
-
-def lista2(ludzie, kraje):
-    assignments = list(zip(ludzie, kraje))
-    """ wypisz wynik
-    for i in assignments:
+def perform_lottery_one(names, countries):
+    print("Lottery begins!")
+    for osoba, kraj in itertools.product(names, countries):
         time.sleep(2)
-        print(i)
-    """
+        print(osoba + " has team from " + kraj)
+    print("Lottery ends!")
+
+
+def connect_two_lists(names, countries):
+    assignments = list(zip(names, countries))
     return assignments
 
 
-def zapisz_wyniki(wynik):
+def save_result(result):
     date_string = time.strftime("%Y-%m-%d_%H%M%S")
-    with open("wyniki{0}.txt".format(date_string), "w") as text_file:
+    with open("result{0}.txt".format(date_string), "w") as text_file:
         for i in range(0, 32):
-            print(f"Para:  {str(i+1)}", file=text_file)
-            print(wynik[i], file=text_file)
-        text_file.close()
+            print(f"Pair:  {str(i+1)}", file=text_file)
+            print(result[i], file=text_file)
+    text_file.close()
     print("Writing done!")
 
 
-"""czyta plik z lista osob bioracych udzial w zabawie oraz lista druzyn"""
-ludzie = czytaj_osoby()
-kraje = czytaj_kraje()
-"""randomowe przesortowanie list"""
-random.shuffle(ludzie)
-random.shuffle(kraje)
-"""polaczenie w pary"""
-wynik = lista2(ludzie, kraje)
-"""zapisanie do pliku"""
-zapisz_wyniki(wynik)
+"""read files with names and countires"""
+names = read_names()
+countries = read_countries()
+"""randomize lists"""
+random.shuffle(names)
+random.shuffle(countries)
+"""connect into pairs"""
+result = connect_two_lists(names, countries)
+"""write to file"""
+save_result(result)
